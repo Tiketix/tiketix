a8aa0f506ec843e667c46dccc19781479ae878c5
clarksonjay <clarksonjubilee35@gmail.com>
2025-02-19 18:46:42 +0100

    Done modifing the Login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,35 +1,69 @@
+"use client"
+
+import { useState } from "react";
 import style from "./login.module.css"
+import { IoMailOutline, IoLockClosedOutline, IoPersonOutline, IoEyeOutline, IoEyeOffOutline, IoLogoFacebook } from "react-icons/io5";
+import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
 
 export default function Login() {
+
+  const[showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const togglePasswordVisibility = () =>{
+    setShowPassword(!showPassword);
+  }
+
   return (
     <div className={style.container}>
 
-      <h1 className={style.name}>TICKETIX</h1>
-      <p className={style.welcome}>Welcome Back!</p>
-      <p className={style.loginMsg}>Use your credentials to access your account</p>
-      <br /><br />
-
+      <div className={style.containerTop}>
+        <h3 className={style.name}>TICKETIX</h3>
+        <h2>Welcome Back!</h2>
+        <p>Use your credentials to access your account</p>
+      </div>
       
-        <div>
+        <div className={style.userInput}>
         
-          <input type = "text" id="email" placeholder="Enter Email Address" className={style.inputBox} required/>
+          <IoPersonOutline className={style.icon}/>
+          <input type ="text" placeholder="Enter Username" required/>
+          <br />
+          <IoLockClosedOutline className={style.icon}/> 
+          <input className={style.lockInput}
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter Password"
+          required
+          />
+
+          <span onClick={togglePasswordVisibility} className={style.eyeIcon}>
+            {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
+          </span>
+          <br />
+
+          <Link href={""} className={style.forgotPassword}>Forgot Password?</Link>
           <br /><br />
 
-          <input type = "text" id="pass" placeholder="Enter Password" className={style.inputBox} required/><br />
-          <p className={style.forgotPassword}>Forgot Password?</p>
-          <br /><br />
+          <button className={style.loginBtn}>Login</button>
+       </div>
+        <p className={style.hr}>or</p>
 
-          <button className={style.loginBtn}>Login</button><br />
-          <hr />
-          <button className={style.loginBtn2}>Login with Facebook</button><br />
-          <button className={style.loginBtn2}>Login with Google</button><br />
-
-          <p className={style.signUp}>Don't have an account? <span><Link href="/registration">Sign up</Link></span></p>
+        <div className={style.socialBtn}>
+          <button>
+           <span><IoLogoFacebook /></span>
+            Login with Facebook
+          </button>
+          <button>
+            <span><FcGoogle /></span>
+            Login with Google
+          </button>
         </div>
-      
+        
+
+        <p className={style.signUp}>Don't have an account? <span><Link href="/registration">Sign up</Link></span></p>
 
     </div>
-  
   );
 }
