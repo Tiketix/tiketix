121c635d57d4fa17ae67c3f146009720f1a46279
Jubilee Clarkson <159916044+clarksonjay@users.noreply.github.com>
2025-03-08 14:49:26 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
-import { Work sans } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-
-const workSans = WorkSans({
-  variable: ["--font-WorkSans"],
+const workSans = Work_Sans({
+  variable: "--font-WorkSans",
   subsets: ["latin"],
-  weight: ["300, 400, 500"]
+  weight: ["300", "400", "500"]
 });
-
-
 
 export const metadata = {
   title: "TICKETIX",
@@ -18,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${workSans.variable}`}
-      >
+      <body className={workSans.variable}>
         {children}
       </body>
     </html>
