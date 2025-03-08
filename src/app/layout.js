12f8f1433a34cb2cfc768396691bf81ffe1713af
Jubilee Clarkson <159916044+clarksonjay@users.noreply.github.com>
2025-03-08 14:51:44 +0100

    Update layout.js
    Just turned the weight from strings to an array and the variable key to a string

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const workSans = Work_Sans({
   variable: "--font-WorkSans",
   subsets: ["latin"],
-  weight: ["300", "400", "500"]
+  weight: ["300", "400", "500", "600", "700", "800", "900"]
 });
 
 export const metadata = {
