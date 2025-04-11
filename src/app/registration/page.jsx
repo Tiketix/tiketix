22ad5434ab7617c4869b4b111f48698abd1cb1ed
Tobi <teesavage077@gmail.com>
2025-04-11 08:28:52 -0400

    testing api

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -1,22 +1,49 @@
 "use client"
 
-import { useState } from 'react';
-import style from "./register.module.css"
-import { IoMailOutline, IoLockClosedOutline, IoPersonOutline, IoEyeOutline, IoEyeOffOutline, IoLogoFacebook } from "react-icons/io5";
+import { useState } from "react";
+import style from "./register.module.css";
+import {
+  IoMailOutline,
+  IoLockClosedOutline,
+  IoPersonOutline,
+  IoEyeOutline,
+  IoEyeOffOutline,
+  IoLogoFacebook,
+} from "react-icons/io5";
 import { FcGoogle } from "react-icons/fc";
-import Link from "next/link"
+import Link from "next/link";
+
 
 export default function Registration() {
   const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState("");
 
   const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword); 
+    setShowPassword(!showPassword);
   };
 
+  const registerUser = async () => {
+    const sampleBody = {
+      firstName: "Tobi",
+      lastName: "Dev",
+      userName: "presidentss",
+      email: "tobiiii@gmail.com",
+      phoneNumber: "08012345678",
+      password: "12345678",
+    };
+
+    const request = await fetch(
+      "http://localhost:5013/api/authentication/register-user",{
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(sampleBody)}
+    );
+
+    console.log(request);
+    console.log(request.message)
+  };
   return (
     <div className={style.wrapper}>
-
       <div className={style.wrapperTop}>
         <h3 className={style.name}>TICKETIX</h3>
         <h2>Create an Account</h2>
@@ -24,13 +51,15 @@ export default function Registration() {
       </div>
 
       <div className={style.userInput}>
-       
-        <IoPersonOutline className={style.icon}/>
-        <input type="text" placeholder="Enter Username" required/><br /> 
-        <IoMailOutline className={style.icon}/>
-        <input type="email" placeholder="Enter Email address" required/><br />
-        <IoLockClosedOutline className={style.icon}/> 
-        <input className={style.lockInput}
+        <IoPersonOutline className={style.icon} />
+        <input type="text" placeholder="Enter Username" required />
+        <br />
+        <IoMailOutline className={style.icon} />
+        <input type="email" placeholder="Enter Email address" required />
+        <br />
+        <IoLockClosedOutline className={style.icon} />
+        <input
+          className={style.lockInput}
           type={showPassword ? "text" : "password"}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -38,23 +67,34 @@ export default function Registration() {
           required
         />
 
-          <span onClick={togglePasswordVisibility} className={style.eyeIcon}>
-            {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
-          </span>
-          <br />
+        <span onClick={togglePasswordVisibility} className={style.eyeIcon}>
+          {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
+        </span>
+        <br />
 
-        <button className={style.signupBtn}>Signup</button>
+        <button onClick={registerUser} className={style.signupBtn}>Signuppp</button>
       </div>
 
       <p className={style.hr}>Or</p>
 
       <div className={style.socialBtn}>
-        <button><span><IoLogoFacebook /></span> Login with Facebook</button>
-        <button><span><FcGoogle /></span> Login with Google</button>
+        <button>
+          <span>
+            <IoLogoFacebook />
+          </span>{" "}
+          Login with Facebook
+        </button>
+        <button>
+          <span>
+            <FcGoogle />
+          </span>{" "}
+          Login with Google
+        </button>
       </div>
 
-      <p className={style.login}>Already have an account? <Link href="/login">Login</Link></p>
+      <p className={style.login}>
+        Already have an account? <Link href="/login">Login</Link>
+      </p>
     </div>
-  
   );
 }
