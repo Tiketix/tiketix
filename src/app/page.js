5c2d55ef40676813d025fc4b62c632da68b430d0
Yinkajay <yinkajunn@gmail.com>
2025-04-15 23:44:03 +0100

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
-    <h2>EVENTS</h2>
-    
+      <h2 className="text-blue-700">EVENTS</h2>
     </div>
-  
+
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h2></h2>
+      <ul>
+        <li>Schedule</li>
+        <li>Speakers</li>
+        <li>Ticket</li>
+        <li>Contact</li>
+        <li>
+            
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
