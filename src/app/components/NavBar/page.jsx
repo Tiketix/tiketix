abd38e206f549bf8de28233c5edf04965666cdb5
clarksonjay <clarksonjubilee35@gmail.com>
2025-05-07 15:13:31 +0100

    updated the navbar

--- src/app/components/NavBar/page.jsx
+++ src/app/components/NavBar/page.jsx
@@ -3,6 +3,14 @@
 import Link from "next/link"
 
 export default function NavBar() {
+  // This component is a navigation bar that contains links to different sections of the page and login/signup buttons.
+  const navItems = [
+    { label: "Schedule", href: "#schedule" },
+    { label: "Speakers", href: "#speakers" },
+    { label: "Ticket", href: "#ticket" },
+    { label: "Contact", href: "#contact" }
+  ];
+
   return (
     <div className="flex justify-between items-center px-8 py-8 text-white bg-[#ff6600] shadow-[0_0_10px_rgba(0,0,0,0.1)] fixed top-0 w-full z-[1000]">
       <h1 className="text-lg font-bold text-white">
@@ -10,10 +18,7 @@ export default function NavBar() {
       </h1>
       <nav>
         <ul className="flex space-x-4">
-          {[{ label: "Schedule", href: "#schedule" },
-            { label: "Speakers", href: "#speakers" },
-            { label: "Ticket", href: "#ticket" },
-            { label: "Contact", href: "#contact" }].map((item) => (
+          {navItems.map((item) => (
             <li key={item.label}>
               <Link href={item.href}>{item.label}</Link>
             </li>
